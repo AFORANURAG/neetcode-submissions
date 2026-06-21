9c7afd451dddf60651bb174513281434e6878bbe
Anurag Upadhyay <108891203+AFORANURAG@users.noreply.github.com>
2026-06-21 19:42:12 +0530

    Add: count-connected-components - submission-0

---
+++ Data Structures & Algorithms/count-connected-components/submission-0.go
@@ -0,0 +1,45 @@
+func countComponents(n int, edges [][]int) int {
+    // how do you calculate number of disconnected components or how many components are there 
+    
+
+	// 1:[2,3]
+	// 2:[4,6]
+	// 3:[9,10]
+	// 11:[15,19]
+	
+	// run dfs starting at 0
+	// if len(visited)==n, output 1
+	// else iterate on map,and skip all those which have been visited
+	// now, you got an edge which has not been visited earlier, now do it again and so on.
+
+	// in actuality, we are iterating two time on each element
+
+	// given: undirected graph
+	adj := make([][]int, n)
+	visited := make([]bool, n)
+	for _, edge := range edges {
+		u, v := edge[0], edge[1]
+		adj[u] = append(adj[u], v)
+		adj[v] = append(adj[v], u)
+	}
+
+	var dfs func(int)
+	dfs = func(node int){
+	  visited[node] = true
+	  for _,nei:=range adj[node]{
+		if !visited[nei]{
+			dfs(nei)
+		}
+	  }
+
+		
+	}
+	res:= 0
+	for node := 0; node < n; node++ {
+		if !visited[node] {
+			dfs(node)
+			res++
+		}
+	}
+	return res
+}
